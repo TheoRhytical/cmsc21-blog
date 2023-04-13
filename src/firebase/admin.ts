95d590289e68a1b2a8fa9f59fea60744d2d45658
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-04-13 13:14:11 +0800

    moved admin apikey to .env

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,6 +1,6 @@
 import { apps, credential } from 'firebase-admin';
 import { initializeApp, getApp } from 'firebase-admin/app';
-import serviceAccount from "@/firebase/cmsc21-blog-firebase-adminsdk-1z0t0-e1315f0237.json";
+// import serviceAccount from "@/firebase/cmsc21-blog-firebase-adminsdk-1z0t0-e1315f0237.json";
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 
@@ -11,9 +11,10 @@ export function getAdminApp() {
 	}
 	return initializeApp({
 		credential: credential.cert({
-			projectId: serviceAccount.project_id,
-			clientEmail: serviceAccount.client_email,
-			privateKey: serviceAccount.private_key.replace(/\\n/g, '\n')
+			projectId: process.env.PROJECT_ID,
+			clientEmail: process.env.CLIENT_EMAIL,
+			privateKey: process.env.PRIVATE_KEY
+			// privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
 		}),
 	});
 }
